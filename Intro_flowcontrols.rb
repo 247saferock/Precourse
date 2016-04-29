dfdaf30c25a9fbec9b37862984344357f2fca304
John P <247saferock@gmail.com>
2016-04-29 01:08:33 -0700

    intro_flowcontrols

---
+++ Intro_flowcontrols.rb
@@ -0,0 +1,11 @@
+# 1. Write down whether the following expressions return true or false. Then type the expressions into irb to see the results.
+
+# 2. Write a method that takes a string as argument. The method should return the all-caps version of the string, only if the string is longer than 10 characters. Example: change "hello world" to "HELLO WORLD". (Hint: Ruby's String class has a few methods that would be helpful. Check the Ruby Docs!)
+
+# 3. Write a program that takes a number from the user between 0 and 100 and reports back whether the number is between 0 and 50, 51 and 100, or above 100.
+
+# 4. What will each block of code below print to the screen? Write your answer on a piece of paper or in a text editor and then run each block of code to see if you were correct.
+
+# 5. Rewrite your program from exercise 3 using a case statement. Wrap the statement from exercise 3 in a method and wrap this new case statement in a method. Make sure they both still work.
+
+# 6. When you run the following code...
