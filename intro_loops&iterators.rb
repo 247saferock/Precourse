f462551ee71fe2943f03812fce15a088b76233e1
John P <247saferock@gmail.com>
2016-04-30 01:46:55 -0700

    intro_loops

---
+++ intro_loops&iterators.rb
@@ -0,0 +1,7 @@
+# 1. What does the each method in the following program return after it is finished executing?
+
+# 2. Write a while loop that takes input from the user, performs an action, and only stops when the user types "STOP". Each loop can get info from the user.
+
+# 3. Use the each_with_index method to iterate through an array of your creation that prints each index and value of the array.
+
+# 4. Write a method that counts down to zero using recursion.
