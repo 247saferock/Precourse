bac47937f51b66ae16b5ffad345d1f51d933e679
John P <247saferock@gmail.com>
2016-04-30 01:42:11 -0700

    intro_flowcontrols

--- Intro_flowcontrols.rb
+++ Intro_flowcontrols.rb
@@ -1,11 +1,68 @@
 # 1. Write down whether the following expressions return true or false. Then type the expressions into irb to see the results.
+1. false
+2. false
+3. false
+4. true
+5. true
 
 # 2. Write a method that takes a string as argument. The method should return the all-caps version of the string, only if the string is longer than 10 characters. Example: change "hello world" to "HELLO WORLD". (Hint: Ruby's String class has a few methods that would be helpful. Check the Ruby Docs!)
+def cap(len)
+  puts len.upcase if len.lenth>10
+end
+
+cap("Johnn Pan is a pimp!")
 
 # 3. Write a program that takes a number from the user between 0 and 100 and reports back whether the number is between 0 and 50, 51 and 100, or above 100.
+puts "Please enter a number between 0 and 100."
+number = gets.chomp.to_i
+
+if number < 0
+  puts "You can't enter a negative number!"
+elsif number <= 50
+  puts "#{number} is between 0 and 50"
+elsif number <= 100
+  puts "#{number} is between 51 and 100"
+else
+  puts "#{number} is above 100"
+end
 
 # 4. What will each block of code below print to the screen? Write your answer on a piece of paper or in a text editor and then run each block of code to see if you were correct.
+1. false
+2. "Did you get it right?"
+3. "Alright now!"
+
 
 # 5. Rewrite your program from exercise 3 using a case statement. Wrap the statement from exercise 3 in a method and wrap this new case statement in a method. Make sure they both still work.
+def evaluate_num(num)
+  if num < 0
+    puts "You can't enter a negative num!"
+  elsif num <= 50
+    puts "#{num} is between 0 and 50"
+  elsif num <= 100
+    puts "#{num} is between 51 and 100"
+  else
+    puts "#{num} is above 100"
+  end
+end
+
+def evaluate_case1_num(num)
+  case
+  when num < 0
+    puts "You can't enter a negative num!"
+  when num <= 50
+    puts "#{num} is between 0 and 50"
+  when num <= 100
+    puts "#{num} is between 51 and 100"
+  else
+    puts "#{num} is above 100"
+  end
+end
+
+puts "Please enter a number between 0 and 100."
+number = gets.chomp.to_i
+
+evaluate_num(number)
+evaluate_case1_num(number)
 
 # 6. When you run the following code...
+missing an extra "end" to match method def.
